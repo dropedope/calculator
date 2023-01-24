4084a1fd0db10af4c97d3d28fc82f564a2a45e03
dropedope <123514853+dropedope@users.noreply.github.com>
2023-01-24 19:16:51 -0300

    Update cacul.py

--- cacul.py
+++ cacul.py
@@ -5,6 +5,7 @@ n2 = st.number_input("Quantidade solicitada: ")
 n3 = (n1*0.10+n1)*n2
 st.write(f'A máquina rodará durante {n3} horas')
 
+import streamlit as st
 st.title('Calculadora de Horas')
 n1 = st.number_input("Quantidade solicitada: ")
 n2 = st.number_input("Peças por hora: ")
